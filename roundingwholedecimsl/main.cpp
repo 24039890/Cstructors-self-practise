aff64890f158fcc0df95233adc323659bf4d1c80
24039890 <163397479+24039890@users.noreply.github.com>
2025-03-25 22:32:26 +0200

    Add files via upload

---
+++ roundingwholedecimsl/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace  std;
+
+int main(){
+
+double  roundnum;
+cout<<"enter  the  value your want to round up :\n";
+cin>>roundnum;
+
+cout<<"rounded number:"<<floor(roundnum);
+
+
+
+
+}
